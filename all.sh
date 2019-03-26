4dadfd106ed7b8ddf12de14567db0b15930ceebb
Hendri Pretorius <pretorh@gmail.com>
2019-03-26 20:45:11 +0200

    rename docker images, use :base as tag
    rename to just android-builder, use :base for the main Dockerfile, and
    use :<sdk-version> for the specific versions

--- all.sh
+++ all.sh
@@ -1,6 +1,7 @@
-docker build -t pretorh/android-builder-base .
+#!/usr/bin/env sh
+docker build -t pretorh/android-builder:base .
 
 for file in Dockerfile.* ; do
-    number=$(echo $file | sed -e 's#Dockerfile.##')
-    docker build -t pretorh/android-builder-$number -f $file .
+    number=$(echo "$file" | sed -e 's#Dockerfile.##')
+    docker build -t pretorh/android-builder:"$number" -f "$file" .
 done
