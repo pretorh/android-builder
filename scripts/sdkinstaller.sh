5f09c721d26396dc164daf3556fb1d4f2a59a28b
Hendri Pretorius <hendri@pretorh.xyz>
2020-03-01 07:47:07 +0200

    scripts: pass sdk root into sdkmanager

--- scripts/sdkinstaller.sh
+++ scripts/sdkinstaller.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "accepting licenses"
-yes | "$ANDROID_HOME/tools/bin/sdkmanager" --licenses
+yes | "$ANDROID_HOME/tools/bin/sdkmanager" --sdk_root="${ANDROID_HOME}" --licenses
 
-echo "installing $@"
-"$ANDROID_HOME/tools/bin/sdkmanager" $@
+echo "installing $*"
+"$ANDROID_HOME/tools/bin/sdkmanager" --sdk_root="${ANDROID_HOME}" "$@"
