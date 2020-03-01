73cf1885e4790a650408d9db6b59f324dbae1b34
Hendri Pretorius <hendri@pretorh.xyz>
2020-03-01 07:28:40 +0200

    build: tag with date and with number only
    when building, tag base and base_<date>, since other docker images are
    built form base

--- all.sh
+++ all.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env sh
+set -e
+
 suffix=$(date --iso-8601)
 
 docker pull pretorh/android-builder:base
-docker build -t pretorh/android-builder:"base_$suffix" .
+docker build \
+    -t "pretorh/android-builder:base_$suffix" \
+    -t "pretorh/android-builder:base" \
+    .
 
 for file in Dockerfile.* ; do
     number=$(echo "$file" | sed -e 's#Dockerfile.##')
     docker pull pretorh/android-builder:"$number"
-    docker build -t pretorh/android-builder:"${number}_${suffix}" -f "$file" .
+    docker build \
+        -t "pretorh/android-builder:${number}_${suffix}" \
+        -t "pretorh/android-builder:${number}" \
+        -f "$file" \
+        .
 done
