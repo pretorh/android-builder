0bb5258c10942ef500817dbba65eb568ea148c38
Hendri Pretorius <pretorh@gmail.com>
2023-10-08 14:40:20 +0200

    build: use dashes as separator in version suffixes of tags

--- all.sh
+++ all.sh
@@ -5,7 +5,7 @@ suffix=$(date +%Y-%m-%d)
 
 docker pull pretorh/android-builder:base
 docker build \
-    -t "pretorh/android-builder:base_$suffix" \
+    -t "pretorh/android-builder:base-$suffix" \
     -t "pretorh/android-builder:base" \
     .
 
@@ -13,7 +13,7 @@ for file in Dockerfile.* ; do
     number=$(echo "$file" | sed -e 's#Dockerfile.##')
     docker pull pretorh/android-builder:"$number"
     docker build \
-        -t "pretorh/android-builder:${number}_${suffix}" \
+        -t "pretorh/android-builder:${number}-${suffix}" \
         -t "pretorh/android-builder:${number}" \
         -f "$file" \
         .
